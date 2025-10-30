9ba2a928f09c4e456dcbb0635195ed00dae0e057
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-30 15:20:23 +0700

    tambah Column layout dengan padding & alignment

--- app/src/main/java/com/example/praktikum4/TextEdit.kt
+++ app/src/main/java/com/example/praktikum4/TextEdit.kt
@@ -17,4 +17,12 @@ fun FormDataDiri(modifier: Modifier) {
 
     val gender: List<String> = listOf("laki-laki", "Perempuan")
 
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+
 }
