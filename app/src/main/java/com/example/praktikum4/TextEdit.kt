e49c28bed9ed78a182a2fb96f06818bfdb0dccfa
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-30 16:05:06 +0700

    tambah core Compose runtime, Modifier, dp, Alignment, Color, resource funcs

--- app/src/main/java/com/example/praktikum4/TextEdit.kt
+++ app/src/main/java/com/example/praktikum4/TextEdit.kt
@@ -1,11 +1,18 @@
 package com.example.praktikum4
 
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+
 
 
 @Composable
