8f319e150c82f0ddc90d72d529569fbe9afe91b8
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-30 15:06:17 +0700

    Tambah padding kecil dan medium

--- app/src/main/java/com/example/praktikum4/TextEdit.kt
+++ app/src/main/java/com/example/praktikum4/TextEdit.kt
@@ -1,2 +1,11 @@
 package com.example.praktikum4
 
+import androidx.compose.runtime.mutableStateOf
+
+
+@Composable
+fun FormDataDiri(modifier: Modifier)
+{
+    var textNama by remember (mutableStateOf(""))
+
+}
