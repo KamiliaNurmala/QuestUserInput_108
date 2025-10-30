966c21cafd88c51c5aa40ff125e643616f4e4649
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-30 16:08:44 +0700

    tambah layout, selectable dan Material3 UI components

--- app/src/main/java/com/example/praktikum4/TextEdit.kt
+++ app/src/main/java/com/example/praktikum4/TextEdit.kt
@@ -13,6 +13,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.selection.selectable
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.CardDefaults
 
 
 @Composable
