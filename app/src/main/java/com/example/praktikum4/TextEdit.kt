eefef771c9c039bc95158a0013883acba0eb0ac5
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-30 15:16:31 +0700

    Tambah input state (textNama, textAlamat, textJK)

--- app/src/main/java/com/example/praktikum4/TextEdit.kt
+++ app/src/main/java/com/example/praktikum4/TextEdit.kt
@@ -5,5 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 
 @Composable
 fun FormDataDiri(modifier: Modifier) {
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
 
+    
 }
